92fc0e6d9d71782b4387e70d504da0e293557720
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-09-25 19:58:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const meInReact = React.createElement('div', {className: 'me'},
+[
+  React.createElement('h1', {}, 'An Awesome Person'),
+  React.createElement('p', {}, 'Who is learning React'),
+  React.createElement('ul', {className: 'me_interests'},[
+    React.createElement('li', {}, 'JavaScript'),
+    React.createElement('li', {}, 'React'),
+    React.createElement('li', {}, 'Movies'),
+    React.createElement('li', {}, 'Ice cream')
+  ])
+])
+
+ReactDOM.render(meInReact, document.getElementById('main'))
 //Code React element here
